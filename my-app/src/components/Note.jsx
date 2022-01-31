4da405e016a49cba3f859a2edf8bcadd44e2604a
shahhardik4599 <hardikshah_2000@yahoo.com>
2022-01-31 20:11:08 +0530

    adding footer and content

---
+++ my-app/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className = "note" >
+            <h1>This is Note Heading</h1>
+            <p>This is Note Content</p>
+        </div>
+    )
+}
+
+export default Note;
