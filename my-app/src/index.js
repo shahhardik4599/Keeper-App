81ab964b55ba5a69c852ae5f18a248f226623164
shahhardik4599 <hardikshah_2000@yahoo.com>
2022-01-31 16:03:52 +0530

    starting file

---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import ReactDOM  from "react-dom";
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+    <h1>Hello world</h1>,
+    document.getElementById("root")
+);
