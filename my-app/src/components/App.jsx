108064aa203372e93bac740274999342db31bedf
shahhardik4599 <hardikshah_2000@yahoo.com>
2022-02-19 15:04:01 +0530

    adding a create area

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -3,12 +3,14 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 
 
 function App(){
    return <div>
         <Header />
+        <CreateArea />
          {notes.map( noteitem => {
                         return <Note
                            key = {noteitem.key}
